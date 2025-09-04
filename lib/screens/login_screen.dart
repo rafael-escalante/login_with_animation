2cd90bb3d79437c6a3d790b49a608504b350816b
rafael-escalante <rafaelescalante0607@gmail.com>
2025-09-04 10:30:58 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,14 +9,63 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<LoginScreen> {
+  bool _obscurePassword = true; // Estado para mostrar/ocultar contraseña
   @override
   Widget build(BuildContext context) {
+    //para obtener el tamaño de la pantalla del disp
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Expanded(child: RiveAnimation.asset("animated_login_character.riv"))
-        ],
+          //margen interior
+          child: Padding(
+        //eje horizontal/derecha izquierda
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset("animated_login_character.riv")),
+            //Espacio entre el oso y el texto email
+            const SizedBox(height: 15),
+            //campo de texto de email
+            SizedBox(
+              width: 370,
+              child: TextField(
+                  //para que aparezca @ en moviles
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: const Icon(Icons.mail),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ))),
+            ),
+            const SizedBox(height: 15),
+            //campo de texto de la contraseña
+            SizedBox(
+              width: 370,
+              child: TextField(
+                  obscureText: _obscurePassword,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscurePassword
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ))),
+            )
+          ],
+        ),
       )),
     );
   }
